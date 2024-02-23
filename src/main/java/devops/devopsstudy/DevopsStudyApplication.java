559f96b1f6871c834ef335116ca5ec64c96a585f
minwoo <keuye06380618@gmail.com>
2024-02-23 23:56:09 +0900

    first commit

---
+++ src/main/java/devops/devopsstudy/DevopsStudyApplication.java
@@ -0,0 +1,13 @@
+package devops.devopsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devops/devopsstudy/DevopsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.devopsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
